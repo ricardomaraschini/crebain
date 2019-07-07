91db24f7a14f2cb2848173c7648961a8ab4114af
Ricardo Maraschini <rmarasch@redhat.com>
2019-07-07 12:47:56 +0200

    Merging filters.go on main.go

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/ricardomaraschini/crebain/fbuffer"
@@ -73,4 +76,24 @@ func test(dirs []string) {
 		cmd.Stderr = os.Stderr
 		cmd.Run()
 	}
+}
+
+// hasTestFile is a filter that returns true if either filePath points to a
+// go test file or points to a go file with correspondent go test file.
+func hasTestFile(filePath string) bool {
+	if filepath.Ext(filePath) != ".go" {
+		return false
+	}
+
+	if strings.HasSuffix(filePath, "_test.go") {
+		return true
+	}
+
+	ext := filepath.Ext(filePath)
+	testFilePath := fmt.Sprintf(
+		"%s_test.go",
+		filePath[0:len(filePath)-len(ext)],
+	)
+	_, err := os.Stat(testFilePath)
+	return err == nil
 }
