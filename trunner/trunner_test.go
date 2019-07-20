484080573de541a24b045af90f50de027be91086
Ricardo Jose Maraschini <rmarasch@localhost.localdomain>
2019-07-20 18:08:39 +0200

    Added test for parseOutput

--- trunner/trunner_test.go
+++ trunner/trunner_test.go
@@ -1,6 +1,8 @@
 package trunner
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -20,5 +22,32 @@ func TestInvalidDir(t *testing.T) {
 
 	if len(res.Out) != 1 {
 		t.Fatal("expected only one output on the result")
+	}
+}
+
+type readerCrash struct{}
+
+func (r *readerCrash) Read([]byte) (int, error) {
+	return 0, fmt.Errorf("reader crash error")
+}
+
+func TestParseOutput(t *testing.T) {
+	r := New()
+
+	content := bytes.NewBuffer([]byte("1\n2\n\n3\n"))
+	lines, err := r.parseOutput(content)
+	if err != nil {
+		t.Fatal("unexpected error parsing output")
+	}
+	if len(lines) != 3 {
+		t.Fatal("invalid number of lines returned by parseOutput")
+	}
+
+	_, err = r.parseOutput(&readerCrash{})
+	if err == nil {
+		t.Fatal("error expected but nil received")
+	}
+	if err.Error() != "reader crash error" {
+		t.Fatalf("unexpected error %q", err)
 	}
 }
